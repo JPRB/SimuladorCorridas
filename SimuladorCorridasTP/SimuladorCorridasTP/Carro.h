177144e610c9d9fb9b86720f0f0ebebb3a6f898f
goncalogui1233 <goncalogui123@outlook.pt>
2019-10-29 20:09:02 +0000

    First files

---
+++ SimuladorCorridasTP/SimuladorCorridasTP/Carro.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Carro {
+	string marca, modelo;
+	char id;
+	int energia, maxenergia;
+	bool movimento, emergencia;
+	int velocidade, maxvelocidade;
+	//Piloto p;
+	bool danificado;
+
+public:
+
+	Carro();
+
+};
